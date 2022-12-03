2f259b4c1b43a5ed29cc01e28a6209792730525f
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2022-12-03 02:18:08 -0500

    Add files via upload

---
+++ RandomMathTest.py
@@ -0,0 +1,14 @@
+# Prints out a list of randomly generated arithmetic statements.
+# Inspired by this forum post: https://smwc.me/1388949
+
+import random
+
+for i in range(1,101): # Change "101" into however many items to generate.
+    a = random.randint(1,9999)
+
+    operator = random.choice(["+","-","*","/"])
+    b = eval(f"{9000} {operator} {a}")
+    print(i, ") ", "9000 ", operator, " ", a, " = ", b, sep = '')
+
+
+# Note to self: Although the eval function is incredibly useful here, let's try not to use it in programs that are meant to take a user input.
