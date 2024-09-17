d7df3ac8afcc6eb3d5f7e9f5f83813500f728c4b
Kevin Barbuto <77207271+KevinBarbuto@users.noreply.github.com>
2024-09-17 13:41:20 -0400

    Update pyramid-generator.js

--- js-algorithms-and-data-structures/pyramid-generator.js
+++ js-algorithms-and-data-structures/pyramid-generator.js
@@ -1,3 +1,22 @@
-let character = "Hello";
-let count = 8;
-let rows = [];
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+const call = padRow();
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
